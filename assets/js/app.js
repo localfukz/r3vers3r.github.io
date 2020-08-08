28fa363f9a8bc1aceb23aa29974df7b9c9dc3587
localfukz <66356739+localfukz@users.noreply.github.com>
2020-08-09 00:12:01 +0700

    fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,8 @@
-// by lumm1t: https://github.com/Lumm1t/obnoxious.club
-
 var _app = function () {
     this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
     this.fadeIn = 4000;
     this.id = 0;
-    this.shouldIgnoreGif = false;
-    this.brandDescription = ["Idiot Addict", "Trashwriter", "Malware Enthusiast", ".NET Reverser", "Pentester", "C# Dev"];
+    this.brandDescription = ["Idiot Addict", "Trashwriter", "Malware Analysis", "Reverser", "Pentester", "C# Dev", "FrontEnd Dev"];
 };
 
 var app = new _app();
