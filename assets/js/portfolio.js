d04034a54127ac848b887bd1b652875163ddc56f
localfukz <66356739+localfukz@users.noreply.github.com>
2020-08-09 00:12:44 +0700

    1337

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,23 +3,28 @@
 (function () {
     var timeouts = [];
 
-    window.mobileAndTabletCheck = function () {
+    window.mobileAndTabletCheck = function () 
+    {
         var check = false;
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
         return check;
     };
-    $(document).ready(function(){
-        if (window.mobileAndTabletCheck()) {
-            $("#background").replaceWith('<div id="background" style="background-image: url("https://skofos.xyz/assets/images/phone.gif");"></div>');
+    $(document).ready(function()
+    {
+        if (window.mobileAndTabletCheck()) 
+        {
+            $("#background").replaceWith('<div id="background" style="background-image: url("https://skofos.xyz/assets/others/phone.gif");"></div>');
 
             app.shouldIgnoreVideo = true;
         } 
     });
-    document.addEventListener('contextmenu', function (event) { 
+    document.addEventListener('contextmenu', function (event) 
+    { 
         event.preventDefault() 
     });
 
-    $(window).on('keydown', function () {
+    $(window).on('keydown', function () 
+    {
         if (event.keyCode == 123)
             return false;
         else if (event.ctrlKey && event.shiftKey && event.keyCode == 73)
@@ -35,7 +40,8 @@
     });
 
     $.fn.extend({
-        animateCss: function (animationName) {
+        animateCss: function (animationName) 
+        {
             var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
             this.addClass('animated ' + animationName).one(animationEnd, function () 
             {
@@ -44,10 +50,12 @@
             return this;
         }
     });
-    var type = function (text, speed, timeout, callback) {
+    var type = function (text, speed, timeout, callback) 
+    {
         timeout = (typeof timeout === "number") ? timeout : [0, callback = timeout];
     
-        setTimeout(function () {
+        setTimeout(function () 
+        {
             var typed = new Typed("#line" + ((app.id !== 10) ? ++app.id : app.id += 10), 
             {
                 strings: text,
@@ -56,55 +64,43 @@
             });
         }, timeout);
     };
-    var clearCursor = function () {
+    var clearCursor = function () 
+    {
         return $("span").siblings(".typed-cursor").css("opacity", "0");
     };
 
-    $('#main').fadeOut(200, function () {
+    $('#main').fadeOut(200, function () 
+    {
             
          $("#main").remove();
 
-        setTimeout(function (){
+        setTimeout(function ()
+        {
             $('.a404').animateCss(app.effects[ Math.floor(Math.random() * app.effects.length)]);
         }, 200);
 
-        type(["Hola, I'm <span style='font-weight: 700; color: #ff00bb;'>Skofos</span>."], 30, function(){
-            clearCursor();
-                type(["Actually, I'm <span style='font-weight: 700; color: #ff00bb;'>Cyber Security Enthusiast</span>."], 30, 500, function(){
-                clearCursor();    
-                    type(["keen interest in <span style='font-weight: 700; color: #ff00bb;'>Reversing</span>, <span style='font-weight: 700; color: #ff00bb;'>Pwn</span>, <span style='font-weight: 700; color: #ff00bb;'>Malware Analysts</span>."], 30, 500, function(){
-                    clearCursor();
-                        type(["Playing <span style='font-weight: 700; color: #ff00bb;'>CTF</span>, and learning some <span style='font-weight: 700; color: #ff00bb;'>Web Development</span>."], 30, 500, function(){
-                        clearCursor();
-                           type(["Here i write about different security stuff, writeups,"], 30, 500, function(){
-                           clearCursor();
-                               type(["opinion, anxiety, confide, etc. in Indonesian."], 30, 500, function(){
-                               clearCursor();
-                                   type(["want to make friends? feel free to contact me. Have a nice day!"], 30, 500, function(){
-                                   clearCursor();
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
-        $('.kontak').animateCss('slideInLeft');
+        $('.kontak').animateCss('slideInDown');
         setTimeout(function(){
-            let whoami = baffle('.whoami', {
+            let whoami = baffle('.whoami', 
+            {
+                characters: "AaBbOoUuTt4807:;",
                 speed: 80
             }).start();
             whoami.reveal(3000, 500);
         }, 200);
-        setTimeout(function(){
+        setTimeout(function()
+        {
             $('.skofos').animateCss('pulse');
-            let b = baffle('.skofos', {
+            let b = baffle('.skofos', 
+            {
+                characters: "SsKkOoFf50",
                 speed: 90
             }).start();
             b.reveal(1000,1500);
             $('.nav').animateCss('slideInLeft');
         },200);
-        setTimeout(function () {
+        setTimeout(function () 
+        {
             var typed = new Typed("#brand", 
             {
                 strings: app.brandDescription,
@@ -113,7 +109,55 @@
                 loop: true
         });
     },3000);
-    setTimeout(function () {
+
+    if($(window).width() < 600)
+    {
+        type(["Hola, I'm <span style='font-weight: 700; color: #ff00bb;'>Skofos</span>."], 30, function(){
+            clearCursor();
+                type(["Actually, I'm <span style='font-weight: 700; color: #ff00bb;'>CyberSec Enthusiast</span>."], 30, 500, function(){
+                clearCursor();    
+                    type(["keen interest in <span style='font-weight: 700; color: #ff00bb;'>Reversing</span>, <span style='font-weight: 700; color: #ff00bb;'>Pwn</span>, <span style='font-weight: 700; color: #ff00bb;'>Malware</span>"], 30, 500, function(){
+                    clearCursor();
+                        type(["<span style='font-weight: 700; color: #ff00bb;'>Analysis</span>. And, Here i write about"], 30, 500, function(){
+                        clearCursor();
+                           type(["different security stuff, writeups,"], 30, 500, function(){
+                           clearCursor();
+                               type(["opinion, anxiety, confide, etc,"], 30, 500, function(){
+                               clearCursor();
+                                    type(["in Indonesian. want to make friends?"], 30, 500, function(){
+                                    clearCursor();
+                                        type(["feel free to contact me. Have a nice day!"], 30, 500, function(){
+                                        clearCursor();
+                                    });
+                                });
+                            });
+                        });                
+                    });
+                });
+            });
+        });
+    } else {
+        type(["Hola, I'm <span style='font-weight: 700; color: #ff00bb;'>Skofos</span>."], 30, function(){
+            clearCursor();
+                type(["Actually, I'm <span style='font-weight: 700; color: #ff00bb;'>CyberSec Enthusiast</span>."], 30, 500, function(){
+                clearCursor();    
+                    type(["keen interest in <span style='font-weight: 700; color: #ff00bb;'>Reversing</span>, <span style='font-weight: 700; color: #ff00bb;'>Pwn</span>, <span style='font-weight: 700; color: #ff00bb;'>Malware Analysts</span>."], 30, 500, function(){
+                    clearCursor();
+                        type(["Here i write about different security stuff, writeups,"], 30, 500, function(){
+                        clearCursor();
+                           type(["opinion, anxiety, confide, etc. in Indonesian."], 30, 500, function(){
+                           clearCursor();
+                               type(["want to make friends? feel free to contact me. Have a nice day!"], 30, 500, function(){
+                               clearCursor();
+                            });
+                        });                
+                    });
+                });
+            });
+        });
+    }
+    setTimeout(function () 
+    {
 //a
         $('.container').fadeIn();
 
