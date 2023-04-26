c895d5e3d7f0c413a8c70f132551f0a27b7f6a34
localfukz <66356739+localfukz@users.noreply.github.com>
2023-04-27 05:34:36 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ var _app = function () {
     this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
     this.fadeIn = 4000;
     this.id = 0;
-    this.brandDescription = ["Waalaikumussalam,", "minal aidzin wal faidzin", "mohon maaf lahir dan batin", "Sudah saja", "fokus pada diri kita masing-masing", "maaf kalau aku", "banyak salah kurangnya dari perkataan maupun perbuatan🙏🏻", "Terimakasih"];
+    this.brandDescription = ["Waalaikumussalam,", "minal aidzin wal faidzin", "mohon maaf lahir dan batin", "Sudah saja", "fokus pada diri kita masing-masing", "maaf kalau aku banyak salah kurangnya ", "dari perkataan maupun perbuatan🙏🏻", "Terimakasih"];
 };
 
 var app = new _app();
