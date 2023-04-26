2a1f062becfe650de95d803d2ee5461a2b6a3c75
localfukz <66356739+localfukz@users.noreply.github.com>
2023-04-27 05:30:18 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ var _app = function () {
     this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
     this.fadeIn = 4000;
     this.id = 0;
-    this.brandDescription = ["Idiot Addict", "Trashwriter", "Malware Analysis", "Reverser", "Pentester", "C# Dev", "FrontEnd Dev"];
+    this.brandDescription = ["Waalaikumussalam,", "minal aidzin wal faidzin", "mohon maaf lahir dan batin", "Sudah saja", "fokus pada diri kita masing-masing", "maaf kalau aku", "banyak salah kurangnya dari perkataan maupun perbuatan🙏🏻", "Terimakasih"];
 };
 
 var app = new _app();
